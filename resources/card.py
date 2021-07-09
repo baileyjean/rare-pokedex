82a95dcd12d2a4b6b60def8bd6876374d69e57a2
Brady Roscher <bradyroscher@yahoo.com>
2021-07-08 22:16:19 -0400

    add card and seller page

--- resources/card.py
+++ resources/card.py
@@ -19,7 +19,7 @@ class Cards(Resource):
 
 class CardDetails(Resource):
     def get(self, id):
-        card = Card.query.find_by_id(id)
+        card = Card.find_by_id(id)
         if not card:
             return {"message": "Card with that ID not found"}, 404
         return card.json(), 201
@@ -32,17 +32,18 @@ class CardDetails(Resource):
         db.session.commit()
         return {"message": "Card Deleted", "payload": id}
 
+
 class CardQuality(Resource):
-    def get(self,quality):
+    def get(self, quality):
         cards = Card.find_by_quality(quality)
         if not cards:
             return {"message": "Cards with that Quality Not Found"}, 404
         return [card.json() for card in cards], 201
 
+
 class CardPrice(Resource):
-    def get(self,price):
+    def get(self, price):
         cards = Card.find_by_price(price)
         if not cards:
             return {"message": "Cards of that Price Not Found"}, 404
         return [card.json() for card in cards], 201
-        
