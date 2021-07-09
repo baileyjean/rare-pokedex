2fce6dba65991df06a3e59bc4e9f343609be25b9
Bailey Jean <79871279+baileyjean@users.noreply.github.com>
2021-07-08 21:37:06 -0700

    Update SellerCard.js with styling stuff
    Again - changing Main version on the repo due to version control errors

--- client/src/components/SellerCard.js
+++ client/src/components/SellerCard.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const SellerCard = (props) => {
   return (
-    <div onClick={() => props.props.history.push(`/user/${props.id}`)}>
-      <img src={props.img} style={{ width: '20vw' }} />
-      <div>{props.name}</div>
+    <div className="hover-div">
+      <img src={props.img} className="hover-div-layer_bottom" />
+      <div className="hover-div-layer_top">
+        <div className="hover-div-text" onClick={() => props.props.history.push(`/user/${props.id}`)}>{props.name}</div>
+      </div>
     </div>
   )
 }
